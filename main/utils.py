d67d4b0135c5286bfc67dfe907d413420d56a135
VennaShiva <125667228+VennaShivay@users.noreply.github.com>
2023-05-02 18:44:07 +0530

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -17,8 +17,8 @@ async def progress_message(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
         progress = "\n{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
+            ''.join(["🔥" for i in range(math.floor(percentage / 5))]),
+            ''.join(["_" for i in range(20 - math.floor(percentage / 5))]))                                  
         tmp = progress + PROGRESS_BAR.format(
             a=round(percentage, 2),
             b=humanbytes(current),
