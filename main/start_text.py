e1a8847f5b2272e76b4934658742eebcaf7dbe41
vennashiva O.L <vennashiva@outlook.com>
2023-05-13 16:07:02 +0530

    Fire Remasterd

--- main/start_text.py
+++ main/start_text.py
@@ -5,11 +5,11 @@ from config import ADMIN
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
-    txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
+    txt="ᴛʜɪꜱ ɪꜱ ᴘᴇʀꜱᴏɴᴀʟ ᴜꜱᴇ ʙᴏᴛ 🙏. ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ? 👇 ᴄʟɪᴄᴋ ᴛʜᴇ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴛᴏ ᴅᴇᴘʟᴏʏ"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/MrMKN/Simple-Rename-Bot")
+        InlineKeyboardButton("✨ ֆօʊʀƈɛ✨ ", url="https://github.com/MrMKN/Simple-Rename-Bot")
         ],[
-        InlineKeyboardButton("🚫 Close", callback_data="del")
+        InlineKeyboardButton("🚫 ᴄʟᴏꜱᴇ", callback_data="del")
     ]])
     if msg.from_user.id != ADMIN:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
@@ -20,10 +20,10 @@ async def start_cmd(bot, msg):
 async def start(bot, msg, cb=True):   
     txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/MrMKN>MrMKN</a></b>"                                     
     button= [[
-        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/mkn_bots_updates")
+        InlineKeyboardButton("🤖 ʙᴏᴛ ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/mkn_bots_updates")
         ],[
-        InlineKeyboardButton("ℹ️ Help", callback_data="help"),
-        InlineKeyboardButton("📡 About", callback_data="about") 
+        InlineKeyboardButton("🪂 ʜᴇʟᴘ", callback_data="help"),
+        InlineKeyboardButton("🔥CₒdₑDₑᵥᵢₗ🔥", callback_data="about") 
     ]]  
     if cb:
         await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
@@ -38,8 +38,8 @@ async def help(bot, msg):
     txt += "/view to see your thumbnail \n"
     txt += "/del to delete your thumbnail"
     button= [[        
-        InlineKeyboardButton("🚫 Close", callback_data="del"),
-        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+        InlineKeyboardButton("🚫 ᴄʟᴏꜱᴇ", callback_data="del"),
+        InlineKeyboardButton("⬅️ ʙᴀᴄᴋ", callback_data="start") 
     ]]  
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True)
 
@@ -47,12 +47,12 @@ async def help(bot, msg):
 @Client.on_callback_query(filters.regex("about"))
 async def about(bot, msg):
     me=await bot.get_me()
-    Master=f"<a href=https://t.me/Mo_Tech_YT>MoTech</a> & <a href=https://t.me/venombotupdates>MhdRzn</a>"  
-    Source="<a href=https://github.com/MrMKN/Simple-Rename-Bot>Click Here</a>"
-    txt=f"<b>Bot Name: {me.mention}\nDeveloper: <a href=https://github.com/MrMKN>MrMKN</a>\nBot Updates: <a href=https://t.me/mkn_bots_updates>Mᴋɴ Bᴏᴛᴢ™</a>\nMy Master's: {Master}\nSource Code: {Source}</b>"                 
+    Master=f"<a href=https://t.me/CodeDevl>@CodeDevl</a>"  
+    Source="<a href=https://github.com/VenbaShivaol/FireRenamer>Click Here</a>"
+    txt=f"<b>Bot Name: {me.mention}\nBot Updates: <a href=https://t.me/VarshiBotz>𝕍𝕒𝕣𝕤𝕙𝕚𝔹𝕠𝕥𝕫™</a>\nMy Master's: {Master}\nSource Code: {Source}</b>"                 
     button= [[        
-        InlineKeyboardButton("🚫 Close", callback_data="del"),
-        InlineKeyboardButton("⬅️ Back", callback_data="start") 
+        InlineKeyboardButton("🚫 ᴄʟᴏꜱᴇ", callback_data="del"),
+        InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data="start") 
     ]]  
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
 
@@ -63,5 +63,8 @@ async def closed(bot, msg):
         await msg.message.delete()
     except:
         return
+     txt=f"ʜᴀɪ {msg.from_user.mention} ꜱᴛᴀʀᴛ ʀᴇɴᴀᴍɪɴɢ..🔥🔥 " 
+     
+     
 
 
